81ffa5d427fe701e4dd19a9c73ed7e057ea4398f
kotmatross28729 <kotmatross228@gmail.com>
2024-05-19 23:39:09 +0300

    version update

--- src/main/java/minechem/reference/Reference.java
+++ src/main/java/minechem/reference/Reference.java
@@ -9,9 +9,5 @@ public class Reference
     public static final String ID = "minechem";
 
     // Main version information that will be displayed in mod listing and for other purposes.
-    public static final String V_MAJOR = "@MAJOR@";
-    public static final String V_MINOR = "@MINOR@";
-    public static final String V_REVIS = "@REVIS@";
-    public static final String V_BUILD = "@BUILD@";
-    public static final String VERSION_FULL = V_MAJOR + "." + V_MINOR + "." + V_REVIS + "." + V_BUILD;
+    public static final String VERSION_FULL = "5.0.9" + " kotmatross edition";
 }
